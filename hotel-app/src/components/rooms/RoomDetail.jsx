a1ff34347eb8c7f105d004147d4b93e7f6bc9364
belen ruiz <beelen.ruiz91@gmail.com>
2023-05-29 00:11:20 -0300

    amenities perfect

--- hotel-app/src/components/rooms/RoomDetail.jsx
+++ hotel-app/src/components/rooms/RoomDetail.jsx
@@ -1,65 +1,56 @@
-import { Stack } from '@mui/material'
-import { Button } from '@mui/material'
-import { Typography } from '@mui/material'
-import { Box } from '@mui/material'
-import React from 'react'
-import { amenities as allAmenities} from "../../utils/amenities"
+import { BoyRounded } from "@mui/icons-material";
+import { Stack } from "@mui/material";
+import { Button } from "@mui/material";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+import React from "react";
+import { amenities as allAmenities } from "../../utils/amenities";
 
 export const RoomDetail = ({ roooms }) => {
+    const title = roooms.title;
+    const capacity = roooms.capacity;
+    const amenities = roooms.amenities;
+    const allows = roooms.allows;
+    const donts = roooms.donts;
+    const description = roooms.description;
+    const price = roooms.price;
+    const imgUrl = roooms.imgUrl;
 
-    const title = roooms.title
-    const capacity = roooms.capacity
-    const amenities = roooms.amenities
-    const allows = roooms.allows
-    const donts = roooms.donts
-    const description = roooms.description
-    const price = roooms.price
-    const imgUrl = roooms.imgUrl
 
-    console.log(allAmenities[0].icon)
-
-   
-    
-  return (
-    
-    <Box>
+    return (
         <Box>
-            <img src={imgUrl} alt="" />
-        </Box>   
-
-
-
-        <Stack>
             <Box>
-                <Typography>Amenities</Typography>
+                <img src={imgUrl} alt="" />
+            </Box>
+
+            <Stack>
                 <Box>
-                    
-                    { allAmenities && allAmenities.filter((services) => {
-                    amenities.map((service) => {
-                        if (service === services.name){
-                            console.log(services.icon)
-                            return <div> {services.icon} </div>
-                        }
-                    })})}
+                    <Typography>Amenities</Typography>
+                    <Box>
+                        {allAmenities &&
+                            allAmenities.map((service) => {
+                                if (amenities.includes(service.name)) {
+                                    return (
+                                        <div key={service.name}>
+                                            {service.icon}
+                                        </div>
+                                    );
+                                }
+                            })}
+                    </Box>
                 </Box>
-                
-            </Box>
 
-
-            <Box>
-                <Typography> ${price}</Typography>
-                <Button 
-                    variant="contained"
-                    color="primary"
-                    >RESERVATION
-                </Button>
-            </Box>
-            <Box>
-                <Typography>Description</Typography>
-                <Typography>{description}</Typography>
-            </Box>
-            
-        </Stack>     
-    </Box>
-  )
-}
+                <Box>
+                    <Typography> ${price}</Typography>
+                    <Button variant="contained" color="primary">
+                        RESERVATION
+                    </Button>
+                </Box>
+                <Box>
+                    <Typography>Description</Typography>
+                    <Typography>{description}</Typography>
+                </Box>
+            </Stack>
+        </Box>
+    );
+};
