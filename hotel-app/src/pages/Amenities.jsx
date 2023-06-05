fcb1cf94c5d5b78d9024861c2cb6050d1eceb96d
belen ruiz <beelen.ruiz91@gmail.com>
2023-06-05 19:15:24 -0300

    style

--- hotel-app/src/pages/Amenities.jsx
+++ hotel-app/src/pages/Amenities.jsx
@@ -1,13 +1,20 @@
-import { Typography, Box } from '@mui/material'
+import { Grid } from '@mui/material'
+import { Container, Typography, Box } from '@mui/material'
 import React from 'react'
+import { Hero } from '../components/hero/Hero'
 import { confort } from "../utils/confort"
 
 export const Amenities = () => {
   return (
-    <div>Amenities
+    <Container sx={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+      
+            
+            <Hero pageTitle="Amenities" imgUrl="https://images.pexels.com/photos/3201921/pexels-photo-3201921.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"/>
 
+
+            <Grid container spacing={2}  sx={{ justifyContent: "space-between" }}>
             {confort.map(( service )=>( service && (
-              <Box>
+              <Grid item xs={4}>
                 <Box>
 
                 {service.icon}
@@ -17,8 +24,9 @@ export const Amenities = () => {
 
                 {service.name}
                 </Typography>
-              </Box>
+              </Grid>
             )))}
-    </div>
+            </Grid>
+    </Container>
   )
 }
