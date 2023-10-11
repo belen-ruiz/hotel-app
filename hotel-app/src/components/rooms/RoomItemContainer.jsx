8e47f47f87413b306e4846300125c9da0d6993c1
belen ruiz <beelen.ruiz91@gmail.com>
2023-10-11 03:03:06 -0300

    flechas ok falta

--- hotel-app/src/components/rooms/RoomItemContainer.jsx
+++ hotel-app/src/components/rooms/RoomItemContainer.jsx
@@ -2,15 +2,27 @@ import { Stack } from "@mui/material";
 import { Container, Typography } from "@mui/material";
 import { Button } from "@mui/material";
 import { Box } from "@mui/material";
-import React from "react";
+import React, { useRef } from "react";
 import { useRoomContext } from "../../context/RoomProvider";
 import { RoomItem } from "./RoomItem";
-import ArrowCircleLeftRoundedIcon from '@mui/icons-material/ArrowCircleLeftRounded';
-import ArrowCircleRightRoundedIcon from '@mui/icons-material/ArrowCircleRightRounded';
+import ArrowCircleLeftRoundedIcon from "@mui/icons-material/ArrowCircleLeftRounded";
+import ArrowCircleRightRoundedIcon from "@mui/icons-material/ArrowCircleRightRounded";
 
 export const RoomItemContainer = ({}) => {
     const { rooms, isList, setIsList } = useRoomContext();
 
+    const containerRef = useRef(null);
+
+    const handleScroll = (direction) => {
+        const container = containerRef.current;
+        if (container) {
+            if (direction === "left") {
+                container.scrollLeft -= 300;
+            } else if (direction === "right") {
+                container.scrollLeft += 300;
+            }
+        }
+    };
     return (
         <Container
             sx={{
@@ -19,27 +31,59 @@ export const RoomItemContainer = ({}) => {
                 height: "80vh",
                 justifyContent: "center",
                 alignItems: "center",
-                 }}>
-
-            <Box sx={{ display: "flex", width: "100%" }}>
-                <Stack spacing={1}
-                    sx={{ width: "40%" }}>
+            }}
+        >
+            <Box sx={{ display: "flex", width: "100%", position: "relative",
+ }}>
+                <Stack spacing={1} sx={{ width: "40%" }}>
                     <p className="subtitle secondary"> Discover our rooms </p>
                     <p className="title primary"> Choose your room </p>
                     <p className="parraph primary">
-                                the Best place to enjoy life place to enjoy life place
-                                to enjoy life place to enjoy.</p>
+                        the Best place to enjoy life place to enjoy life place
+                        to enjoy life place to enjoy.
+                    </p>
                 </Stack>
+
+                {/* buttons */}
+                <Box
+                        sx={{
+                            width: "65%",
+                            top: "45%",
+                            display: "flex",
+                            justifyContent: "space-between",
+                            left: "24rem",
+                            position: "absolute",
+                            //right: "8rem",
+                            //transform: "translateY(-50%)",
+                            zIndex: 2000,
+                        }}>
+                        <Box
+                            sx={{ color: "white", cursor: "pointer" }}
+                            onClick={() => handleScroll("left")}
+                        >
+                            <ArrowCircleLeftRoundedIcon fontSize="large" />
+                        </Box>
+                        <Box
+                            sx={{ color: "white", cursor: "pointer" }}
+                            onClick={() => handleScroll("right")}
+                        >
+                            <ArrowCircleRightRoundedIcon fontSize="large" />
+                        </Box>
+                    </Box>
 
                 {/* scroll */}
                 <Container
+                    ref={containerRef}
                     sx={{
                         overflowX: "scroll",
                         scrollBehavior: "smooth",
                         scrollSnapType: "x mandatory",
                         scrollbarWidth: "none",
                         height: "100%",
-                        maxHeight: "70vh"}} >
+                        maxHeight: "70vh",
+                        position: "relative",
+                    }}>
+                    
 
                     <Box
                         sx={{
@@ -48,34 +92,13 @@ export const RoomItemContainer = ({}) => {
                             gap: "1rem",
                             height: "70vh",
                             minHeight: "60vh",
-                            position: "relative"}}>
-
-
-                        {/* buttons */}
-                        <Box sx={{
-                            width: "100%",
-                            top: "50%",
-                            display: "flex",
-                            justifyContent: "space-between",
-                            left: "1rem",
-                            position: "absolute",
-                            right: "8rem",
-                            transform: "translateY(-50%)",
-                            zIndex: 2000 }} > 
-                        
-                            <Box sx={{ color: 'white', }}>
-                                <ArrowCircleLeftRoundedIcon fontSize="large"/> 
-                            </Box>
-                            <Box sx={{ color: 'white', }}>
-                                <ArrowCircleRightRoundedIcon fontSize="large"/> 
-                            </Box>
-
-                        </Box>
-
+                            position: "relative",
+                        }}
+                    >
                         {/* room list */}
                         {rooms &&
-                        rooms.map((room) => (
-                        <Box key={rooms.title}>
+                            rooms.map((room) => (
+                                <Box key={rooms.title}>
                                     {room && (
                                         <RoomItem
                                             isList={isList}
@@ -83,11 +106,11 @@ export const RoomItemContainer = ({}) => {
                                             rooms={room}
                                         />
                                     )}
-                        </Box>))}
-                    </Box>                   
+                                </Box>
+                            ))}
+                    </Box>
                 </Container>
             </Box>
-
         </Container>
     );
 };
