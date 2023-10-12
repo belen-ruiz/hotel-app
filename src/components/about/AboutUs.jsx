67ddca7cad6e6c367ff454b4eb87647c45d20fca
belen ruiz <beelen.ruiz91@gmail.com>
2023-10-12 16:33:44 -0300

    about us

--- src/components/about/AboutUs.jsx
+++ src/components/about/AboutUs.jsx
@@ -1,8 +1,13 @@
 import { Button } from "@mui/material";
 import { Grid, Container, Stack, Box } from "@mui/material";
 import { Link } from "react-router-dom";
-import { TopText, Title, SubtitleJustify, SubtitleBold, SubtitleItalic } from "../../custom/typography/Typography"
-
+import {
+    TopText,
+    Title,
+    SubtitleJustify,
+    SubtitleBold,
+    SubtitleItalic,
+} from "../../custom/typography/Typography";
 
 export const AboutUs = () => {
     return (
@@ -14,25 +19,39 @@ export const AboutUs = () => {
                 alignItems: "center",
             }}
         >
-            <Box sx={{ display: "flex", width: "100%", gap: "1rem", height: "80%" }}>
-                <Stack spacing={1} sx={{ width: "30%" }}>
-                    <p style={ TopText } > a little about us </p>
-                    <p style={ Title }>the Best place to enjoy life </p>
-                    <p style={ SubtitleJustify }>
+            <Box
+                sx={{
+                    display: "flex",
+                    width: "100%",
+                    gap: "1rem",
+                    //height: "80%",
+                }}
+            >
+                {/* titles */}
+
+                <Stack 
+                    spacing={1} 
+                    sx={{ width: "30%",                         alignItems: "start",
+                }}>
+                    <p style={TopText}> a little about us </p>
+                    <p style={Title}>the Best place to enjoy life </p>
+                    <p style={SubtitleJustify}>
                         the Best place to enjoy life place to enjoy life place
                         to enjoy life place to enjoy life place to enjoy life
                         place to enjoy life place to enjoy life place to enjoy
                         life place to enjoy life.
                     </p>
-                    <p style={ SubtitleBold }> FRANCO MOLINA </p>
-                    <p style={ SubtitleItalic }> CEO of Las Salinas </p>
+                    <p style={SubtitleBold}> FRANCO MOLINA </p>
+                    <p style={SubtitleItalic}> CEO of Las Salinas </p>
                 </Stack>
 
-                <Box sx={{ display: "flex", width: "70%"}}>
+                {/* img */}
+
+                <Box sx={{ display: "flex", width: "70%" }}>
                     <img
-                        src="https://images.pexels.com/photos/189296/pexels-photo-189296.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+                        src="https://images.pexels.com/photos/189296/pexels-photo-189296.jpeg?auto=compress&cs=tinysrgb&dpr=1"
                         alt="who we are"
-                        style={{ height: "100%", width: "100%" }}
+                        style={{ width: "100%", objectFit:"cover" }}
                     />
                 </Box>
             </Box>
--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -4,8 +4,13 @@ import { Typography } from "@mui/material";
 import { Toolbar } from "@mui/material";
 import { Container } from "@mui/material";
 import { features } from "../../utils/features";
-import { TopText, Title, Quote, SubtitleBold, SubtitleItalic } from "../../custom/typography/Typography"
-
+import {
+    TopText,
+    Title,
+    Quote,
+    SubtitleBold,
+    SubtitleItalic,
+} from "../../custom/typography/Typography";
 
 export const Testimonials = () => {
     return (
@@ -15,44 +20,55 @@ export const Testimonials = () => {
                 //height: "80vh",
                 justifyContent: "center",
                 alignItems: "center",
-            }}>
+            }}
+        >
             <Stack
+                spacing={1}
                 sx={{
+                    display: "flex",
                     width: "100%",
-                    display: "flex",
                     alignItems: "center",
-                    gap: "1rem",
-                }}>
+                }}
+            >
+                {/* titles */}
 
-                <Stack spacing={1} sx={{
-                    display: "flex",
-                    alignItems: "center",
-                }}>
-                    <p style={TopText} >TESTIMONIALS</p>
+                <Stack
+                    spacing={1}
+                    sx={{
+                        display: "flex",
+                        alignItems: "center",
+                    }}
+                >
+                    <p style={TopText}>TESTIMONIALS</p>
                     <p style={Title}>What our clients say</p>
                 </Stack>
+
+                {/* tripAd quote */}
                 <Stack
                     spacing={1}
                     sx={{
                         display: "flex",
                         width: "80%",
                         alignItems: "center",
-
                     }}
                 >
-                    <p style={ Quote }>
-                        "My stay was simply amazing and exceeded all my expectations. The room, elegantly decorated, offered breathtaking views and exceptional comfort. The pool, located right by the shore, was an oasis of relaxation. The staff, friendly, attentive, and helpful at all times, made every effort to provide me with an unforgettable experience. I would highly recommend this hotel without hesitation for a dream vacation by the beach!"
+                    <p style={Quote}>
+                        "My stay was simply amazing and exceeded all my
+                        expectations. The room, elegantly decorated, offered
+                        breathtaking views and exceptional comfort. The pool,
+                        located right by the shore, was an oasis of relaxation.
+                        The staff, friendly, attentive, and helpful at all
+                        times, made every effort to provide me with an
+                        unforgettable experience. I would highly recommend this
+                        hotel without hesitation for a dream vacation by the
+                        beach!"
                     </p>
-                    <p style={ SubtitleBold }>
-                        Bruce Wayne
-                    </p>
-                    <p style={ SubtitleItalic }>
-                        Reviewed from TripAdvisor
-                    </p>
+                    <p style={SubtitleBold}>Bruce Wayne</p>
+                    <p style={SubtitleItalic}>Reviewed from TripAdvisor</p>
                     <img
                         src="https://download.logo.wine/logo/TripAdvisor/TripAdvisor-Logo.wine.png"
                         alt="trip advisor"
-                        style={{ width: "150px" }} 
+                        style={{ width: "150px" }}
                     />
                 </Stack>
             </Stack>
